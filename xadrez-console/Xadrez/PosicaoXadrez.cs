aa805f6552e8f60107a198a05e79b2e0bd525728
Mizuki <mizukikatsukawa@gmail.com>
2022-01-23 10:48:25 -0400

    Criando classe PosicaoXadrez

---
+++ xadrez-console/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,26 @@
+﻿using Tab;
+
+namespace Xadrez
+{
+    class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}
