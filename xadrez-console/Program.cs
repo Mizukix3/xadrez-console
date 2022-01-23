93e31da80ab77f65e535c3062e92139bc9edd946
Mizuki <mizukikatsukawa@gmail.com>
2022-01-23 10:42:30 -0400

    Tratando excecoes

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,13 +7,20 @@ namespace xadrez_console {
     {
         static void Main(string[] args)
         {
-            Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-            tabuleiro.ColocarPeca(new Torre(Cor.Preto, tabuleiro), new Posicao(0,0));
-            tabuleiro.ColocarPeca(new Torre(Cor.Preto, tabuleiro), new Posicao(1, 3));
-            tabuleiro.ColocarPeca(new Rei(Cor.Preto, tabuleiro), new Posicao(2, 4));
+                tabuleiro.ColocarPeca(new Torre(Cor.Preto, tabuleiro), new Posicao(0, 0));
+                tabuleiro.ColocarPeca(new Torre(Cor.Preto, tabuleiro), new Posicao(1, 3));
+                tabuleiro.ColocarPeca(new Rei(Cor.Preto, tabuleiro), new Posicao(2, 4));
 
-            Tela.ImprimirTabuleiro(tabuleiro);
+                Tela.ImprimirTabuleiro(tabuleiro);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
---
+++ xadrez-console/Tab/TabuleiroException.cs
@@ -0,0 +1,9 @@
+﻿namespace Tab
+{
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+        }
+    }
+}
